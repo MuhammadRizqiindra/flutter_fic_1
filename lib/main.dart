15bd98d3f308781ea80cb5c8b40da1f4bae39387
MuhammadRizqiindra <hudariski578@gmail.com>
2023-12-15 14:25:26 +0700

    meet 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_fic_1/pages/auth/auth_page.dart';
-import 'package:flutter_fic_1/pages/splash/splash_page.dart';
-import 'package:flutter_fic_1/utils/light_themes.dart';
+//import 'package:flutter_seller_app/bloc/add_image/add_image_bloc.dart';
+//import 'package:flutter_seller_app/bloc/bloc/add_product_bloc.dart';
+//import 'package:flutter_seller_app/bloc/categories/categories_bloc.dart';
+//import 'package:flutter_seller_app/bloc/products/products_bloc.dart';
+//import 'package:flutter_seller_app/pages/dashboard/seller_dashboard_page.dart';
 
+import 'bloc/login/login_bloc.dart';
+import 'bloc/logout/logout_bloc.dart';
 import 'bloc/register/register_bloc.dart';
+import 'data/datasources/auth_local_datasource.dart';
+import 'pages/auth/auth_page.dart';
+import 'pages/dashboard/dashboard_page.dart';
+import 'utils/light_themes.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const SellerDashboardPage(),
+//     );
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => RegisterBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RegisterBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LoginBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LogoutBloc(),
+        ),
+      ],
       child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: light,
-        home: const AuthPage(),
-      ),
+          title: 'Flutter Demo',
+          theme: light,
+          home: FutureBuilder<bool>(
+            future: AuthLocalDatasource().isLogin(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              } else if (snapshot.hasData && snapshot.data!) {
+                return const DashboardPage();
+              } else {
+                return const AuthPage();
+              }
+            },
+          )),
     );
   }
 }
